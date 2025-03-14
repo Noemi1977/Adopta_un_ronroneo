f2d612f8b56e50210c487db876b16753ed3c8c4b
SaldaDani <saldadaniel88@gmail.com>
2025-03-14 14:25:48 +0100

    styles logo

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,50 +1,56 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 import "./NavBar.css";
+import logo from "/Logo.png";
 
 const NavBar = () => {
   return (
     <nav className="navbar">
-        <NavLink
+      <NavLink to="/" className="logo-link">
+        <div className="logo-container">
+          <img src={logo} alt="Logo" className="logo" />
+        </div>
+      </NavLink>
+
+      <NavLink
         className="nav-link"
-          to="/"
-          style={({ isActive }) => ({
-            fontWeight: isActive ? "bold" : "normal",
-          })}
-        >
-          Inicio
-        </NavLink>
-        {" | "}
-        <NavLink
+        to="/"
+        style={({ isActive }) => ({
+          fontWeight: isActive ? "bold" : "normal",
+        })}
+      >
+        Inicio
+      </NavLink>
+
+      <NavLink
         className="nav-link"
-          to="/Adopta"
-          style={({ isActive }) => ({
-            fontWeight: isActive ? "bold" : "normal",
-          })}
-        >
-          Adopta
-        </NavLink>
-        {" | "}
-        <NavLink
+        to="/Adopta"
+        style={({ isActive }) => ({
+          fontWeight: isActive ? "bold" : "normal",
+        })}
+      >
+        Adopta
+      </NavLink>
+
+      <NavLink
         className="nav-link"
-          to="/Sobrenosotros"
-          style={({ isActive }) => ({
-            fontWeight: isActive ? "bold" : "normal",
-          })}
-        >
-          Sobre Nosotros
-        </NavLink>
-        {" | "}
-        <NavLink
+        to="/Sobrenosotros"
+        style={({ isActive }) => ({
+          fontWeight: isActive ? "bold" : "normal",
+        })}
+      >
+        Sobre Nosotros
+      </NavLink>
+
+      <NavLink
         className="nav-link"
-          to="/Contacto"
-          style={({ isActive }) => ({
-            fontWeight: isActive ? "bold" : "normal",
-          })}
-        >
-          Contacto
-        </NavLink>
-      
+        to="/Contacto"
+        style={({ isActive }) => ({
+          fontWeight: isActive ? "bold" : "normal",
+        })}
+      >
+        Contacto
+      </NavLink>
     </nav>
   );
 };
