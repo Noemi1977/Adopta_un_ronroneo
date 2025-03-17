6cb128fe3032db13b54c37cd90bca0f91af7c9fb
SaldaDani <saldadaniel88@gmail.com>
2025-03-17 23:31:35 +0100

    style navbar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,31 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
 import "./NavBar.css";
 import logo from "/Logo.png";
 
 const NavBar = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
   return (
     <nav className="navbar">
-    <NavLink to="/" className="logo-container">
-      <img src={logo} alt="Logo" className="logo" />
-    </NavLink>
-    <div className="nav-links">
-      <NavLink className="nav-link" to="/" activeclassname="active">
-        Inicio
+      <NavLink to="/" className="logo-container">
+        <img src={logo} alt="Logo" className="logo" />
       </NavLink>
-      <NavLink className="nav-link" to="/Adopta" activeclassname="active">
-        Adopta
-      </NavLink>
-      <NavLink className="nav-link" to="/Sobrenosotros" activeclassname="active">
-        Sobre Nosotros
-      </NavLink>
-      <NavLink className="nav-link" to="/Contacto" activeclassname="active">
-        Contacto
-      </NavLink>
-    </div>
-  </nav>
-);
-};
 
+      {/* Botón hamburguesa */}
+      <button className="menu-toggle" onClick={() => setMenuOpen(!menuOpen)}>
+        ☰
+      </button>
+
+      {/* Enlaces de navegación */}
+      <div className={`nav-links ${menuOpen ? "open" : ""}`}>
+        <NavLink className="nav-link" to="/" activeclassname="active" onClick={() => setMenuOpen(false)}>
+          Inicio
+        </NavLink>
+        <NavLink className="nav-link" to="/Adopta" activeclassname="active" onClick={() => setMenuOpen(false)}>
+          Adopta
+        </NavLink>
+        <NavLink className="nav-link" to="/Sobrenosotros" activeclassname="active" onClick={() => setMenuOpen(false)}>
+          Sobre Nosotros
+        </NavLink>
+        <NavLink className="nav-link" to="/Contacto" activeclassname="active" onClick={() => setMenuOpen(false)}>
+          Contacto
+        </NavLink>
+      </div>
+    </nav>
+  );
+};
 
 export default NavBar;
