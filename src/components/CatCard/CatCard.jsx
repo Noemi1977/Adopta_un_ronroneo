e0b9532e0387dd1ec1799f6dbc7b0c6bb9ab1d61
Noemi1977 <noemi.casaprima.pendas@gmail.com>
2025-03-17 15:04:14 +0100

    add form

--- src/components/CatCard/CatCard.jsx
+++ src/components/CatCard/CatCard.jsx
@@ -1,34 +1,29 @@
 import { useNavigate } from "react-router-dom";
-import "./CatCard.css"; 
+import "./CatCard.css";
 
 function CatsCard({ cat }) {
   const navigate = useNavigate();
-
+  
   const handleAdoptClick = () => {
-    navigate("/construction"); // Redirige a la página en construcción
+    navigate("/Contacto"); // Redirige a la página de contacto
   };
+
   if (!cat) {
     return <div>No hay información del gato disponible.</div>;
   }
-    return (
-      <div className="cat-card">
-        <div className="image-container">
-          <img src={cat.url} alt="Cat" className="cat-image" />
-        </div>
-        
-        <div>
-          
-            <button className="adopt-button"
-          onClick={handleAdoptClick} 
-        >
-          Adoptar
-          </button>
-        
-        </div>
+
+  return (
+    <div className="cat-card">
+      <div className="image-container">
+        <img src={cat.url} alt="Cat" className="cat-image" />
       </div>
-    );
-  }
-  
-  export default CatsCard;
-  
-  
+      <div>
+        <button className="adopt-button" onClick={handleAdoptClick}>
+          Adoptar
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default CatsCard;
